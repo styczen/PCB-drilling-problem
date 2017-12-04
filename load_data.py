edc2d118650cc38143915187fe7a483d3e65f0a4
Daniel Hyjek <daniel.hyjek@gmail.com>
2017-12-04 16:30:23 +0100

    Modyfikacja kodu do postaci klasy

--- load_data.py
+++ load_data.py
@@ -1,33 +1,44 @@
 import math                         # pozwala na operacje matematyczne typu sqrt
 import matplotlib.pyplot as plt     # do wykresów(składnia jak plot w matlabie)
 import csv                          # do wczytywania lini z pliku jako rekordy
-
-x = []  #wsp x pkt
-y = []  #wsp y pkt
-
-with open('pcb.txt','r') as csvfile:    # pcb.txt to plik ze wspolrzednymi
-    plots = csv.reader(csvfile, delimiter=' ')
-    for row in plots:
-        x.append(int(row[0])/100)   # dodajemy do listy wczytywane współrzędne
-        y.append(int(row[1])/100)   # dzielimy przez 100 aby wspolrzzedne byly w mm
+import numpy as np
 
 
-# ustawienia wykresu płytki
-plt.plot(x,y,'.')
-plt.xlabel(u'Współrzedne X')
-plt.ylabel(u'Współrzedne Y')
-plt.title('Przykładowa płytka PCB')
+class Pcb():
+
+    def __init__(self,file_name):
+        self.D = np.zeros((2,2))
+        self.file = open(file_name,"a+")
+        self.x = []  # wsp x pkt
+        self.y = []  # wsp y pkt
+
+    def __del__(self):
+        self.file.close()
+
+    def read_data(self):
+        with open('pcb.txt','r') as csvfile:  # pcb.txt to plik ze wspolrzednymi
+            points = csv.reader(csvfile, delimiter=' ')
+            for point in points:
+                self.x.append(int(point[0]) / 100)  # dodajemy do listy wczytywane współrzędne
+                self.y.append(int(point[1]) / 100)  # dzielimy przez 100 aby wspolrzzedne byly w mm
+
+    def distance_matrix(self):
+        self.read_data()
+        D = np.zeros((len(self.x),len(self.x)))
+        for i in range(len(self.x)):
+            for j in range(len(self.x)):
+                D[i][j] = math.sqrt((self.x[j] - self.x[i]) ** 2 + (self.y[j] - self.y[i]) ** 2)
+        self.D = D
+        return D
+
+    def show(self):
+        plt.plot(self.x, self.y, '.')
+        plt.xlabel(u'Współrzedne X')
+        plt.ylabel(u'Współrzedne Y')
+        plt.title('Przykładowa płytka PCB')
+        plt.show()
 
 
-D = [[0 for i in range(len(x))] for j in range(len(x))]   # tworzy tablicę na odległości wypełnianą zerami
-
-for i in range(len(x)):
-    for j in range(len(x)):
-        D[i][j] = math.sqrt((x[j] - x[i]) ** 2 + (y[j] - y[i]) ** 2)    # liczy odległości między pkt w
-                                                                        # układzie kartezjańskim
-
-plt.show()  # wyświetla wygląd płytki
-
-
-
-
+dd=Pcb("pcb.txt")
+matrix = dd.distance_matrix()
+print(matrix[0][2])
